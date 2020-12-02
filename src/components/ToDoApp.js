cc2922b79ea7251b96d874812174fd033ba1e49e
Mateo Ramirez Uribe <mateo.ramirez.uribe@gmail.com>
2020-12-02 13:47:12 -0500

    routing is implemented

--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -1,9 +1,65 @@
+import { useEffect, useContext } from 'react';
+import PropTypes from "prop-types";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Login from './Login';
 import ToDoList from './ToDoList'
+import { toDoContext } from "../context/ToDoContext";
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return(
+    <Route
+      {...rest}
+      render={(props) => {
+      return rest.isLoged ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: props.location }} />
+      );
+    }}
+    />
+  );
+};
+
+const PublicRoute = ({ component: Component, restricted, ...rest }) => {
+  return(
+    <Route
+      {...rest}
+      render={(props) => {
+      return rest.isLoged && restricted ? (
+        <Redirect to={{ pathname: "/tasks", state: props.location }} />
+      ) : (
+        <Component {...props} />
+      );
+    }}
+    />
+  );
+};
 const ToDoApp = () => {
+  const context = useContext(toDoContext);
+  const { accessToken, initialAuthVerification } = context;
+
+  useEffect(() => {
+    initialAuthVerification();
+  }, []);
+
   return (
-    <ToDoList />
+    <div>
+      <Switch>
+        <PublicRoute
+            isLoged={accessToken !== null}
+            path="/"
+            restricted={true}
+            component={Login}
+            exact
+          />
+        <PrivateRoute
+          isLoged={accessToken !== null}
+          path="/tasks"
+          component={ToDoList}
+          exact
+        />
+      </Switch>
+    </div>
   );
 }
 
